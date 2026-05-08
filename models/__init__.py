e48079eed26b503f0f37b7855081232a48ed0c9e
Reflective Lantern <devneatharva@gmail.com>
2026-05-08 10:18:01 +0000

    feat(1/60): add models package with Yelp record types

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+"""Pydantic models for Yelp Open Dataset records."""
+
+from __future__ import annotations
+
+from models.yelp import YelpBusiness, YelpReview, YelpUser
+
+__all__ = ["YelpReview", "YelpBusiness", "YelpUser"]
