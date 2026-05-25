71ef69c93696421973697b379fae0c57b3028e8d
Reflective Lantern <devneatharva@gmail.com>
2026-05-25 10:37:37 +0000

    chore(37/60): update pipeline/__init__.py to export RecordJoiner, transformer classes, CleanerStats

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -3,10 +3,12 @@
 from __future__ import annotations
 
 from pipeline.aggregator import StatsAggregator
-from pipeline.cleaner import TextCleaner
+from pipeline.cleaner import CleanerStats, TextCleaner
 from pipeline.exporter import DataExporter
+from pipeline.joiner import RecordJoiner
 from pipeline.processor import RecordProcessor
 from pipeline.sentiment import SentimentAnalyzer
+from pipeline.transformer import ComputedFieldAdder, FieldRenamer, TypeCoercer
 
 __all__ = [
     "RecordProcessor",
@@ -14,4 +16,9 @@ __all__ = [
     "StatsAggregator",
     "DataExporter",
     "TextCleaner",
+    "CleanerStats",
+    "RecordJoiner",
+    "FieldRenamer",
+    "TypeCoercer",
+    "ComputedFieldAdder",
 ]
